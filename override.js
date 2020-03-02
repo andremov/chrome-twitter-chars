d91e97fa3058870fdacf857812f775ddffd43f81
Andres-XL <amovilla1103@gmail.com>
2020-03-02 14:00:50 -0500

    improved class handling of element

--- override.js
+++ override.js
@@ -24,12 +24,17 @@ function checkEdit() {
 function createElement(chars) {
     let elem = document.createElement("DIV");
     elem.id = 'charCount';
-    elem.classList.add('css-901oao');
-    elem.classList.add('r-111h2gw');
-    elem.classList.add('r-n6v787');
-    elem.classList.add('r-19jlu03');
-    elem.classList.add('r-285fr0');
-    elem.classList.add('r-q4m81j');
+    let classes = [
+        'css-901oao',
+        'r-111h2gw',
+        'r-n6v787',
+        'r-19jlu03',
+        'r-285fr0',
+        'r-q4m81j'
+    ];
+    for (let i in classes) {
+        elem.classList.add(classes[i]);
+    }
     let remChars = 280 - chars;
     elem.appendChild(document.createTextNode(''+remChars));
     return elem;
